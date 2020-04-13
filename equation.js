b5b70dc756e401048967bb3446477ca994f74f41
HR1Aventum <rohan.arya237@gmail.com>
2020-04-13 06:07:56 +0530

    Create equation.js

---
+++ equation.js
@@ -0,0 +1,14 @@
+const readline = require('readline')
+const inp = readline.createInterface({ input:process.stdin})
+
+
+inp.on("line", (data) => {value_y(data)});
+
+function value_y(data){
+   var y;
+   var num=[];
+   num = data.split(" ")
+   y=(Number(data[0])*Number(data[1]))+Number(data[2])
+    
+console.log(y)
+}
