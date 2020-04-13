a2abe56f77a79a049331ba12834cf7d0596ed153
HR1Aventum <rohan.arya237@gmail.com>
2020-04-14 01:31:10 +0530

    Update sum_square.js

--- sum_square.js
+++ sum_square.js
@@ -1,14 +1,23 @@
 const rl = require('readline');
 const ip = rl.createInterface({input:process.stdin, output:process.stdout});
 
-ip.on('line', (data) => {(console.log(Sum_square(data)))});
+ip.on('line', (data) => {(Sum_square(data))});
 
 function Sum_square(data){
 var num =[];
-var sum = 0;
+var sum= 0;
+var n = 1;
+var temp = [];
 num = data.split("").map(e => Number(e))
-sum = (num[0]*num[0])+(num[1]*num[1])
-return sum 
+for(var i = 0; i<num.length; i++){
+ n = num[i]*num[i]   
+temp.push(n)
+}
+for (var j = 0; j<temp.length ; j++){
+    sum = sum + temp[j]
 
 }
+console.log(sum)
+}
+
 
